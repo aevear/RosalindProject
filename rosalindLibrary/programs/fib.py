6414e9575dc956370482b02541b94520c76d6899
xan <xan.hahn@gmail.com>
2018-10-05 18:11:48 -0500

    updates

--- rosalindLibrary/programs/fib.py
+++ rosalindLibrary/programs/fib.py
@@ -4,18 +4,16 @@
 
 def runFib(inputFile):
     fi = open(inputFile, 'r') #reads in the file that list the before/after file names
-    activityFile = fi.readline().split() #reads in files
-    n, m = int(activityFile[0]), int(activityFile[1])
+    inputData = fi.readline().split() #reads in files
+    n, m = int(inputData[0]), int(inputData[1])
 
-    sequence = list()
-    mature, immature, babies = 0, 1, 0
+    mature, immature = 0, 1
 
     for i in range(n-1):
-        #Make Babies
         newbabies = mature * m
         mature = immature + mature
         immature = newbabies
-        total = mature + immature + babies
+        total = mature + immature
     return total
 
 
--- rosalindLibrary/programs/iprb.py
+++ rosalindLibrary/programs/iprb.py
@@ -10,10 +10,10 @@ def runIprb(inputFile):
     total = k + m + n
 
     r_r = (n / total) * ((n - 1) / (total - 1))
-    h_h = (m / total) * ((m - 1) / (total - 1))
-    h_r = (m / total) * (n / (total - 1)) + (n / total) * (m / (total - 1))
+    d_d = (m / total) * ((m - 1) / (total - 1))
+    d_r = (m / total) * (n / (total - 1)) + (n / total) * (m / (total - 1))
 
-    k_total = r_r + h_h * 1/4 + h_r * 1/2
+    k_total = r_r + d_d * 1/4 + d_r * 1/2
     k_total =  1 - k_total
 
     return str(k_total)
