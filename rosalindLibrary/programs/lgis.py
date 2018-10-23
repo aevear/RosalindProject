8409a32b604e3d42b7f5f4f7d59b07c73e606230
xan <xan.hahn@gmail.com>
2018-10-23 18:04:48 -0500

    working on lgis

--- rosalindLibrary/programs/lgis.py
+++ rosalindLibrary/programs/lgis.py
@@ -1,26 +1,44 @@
 #-------------------------------------------------------------------------------
 # Lgis
 #-------------------------------------------------------------------------------
+global maximum
+global minimum
+
+def dynamicLoop(arr , n ):
+    # to allow the access of global variable
+    global maximum
+
+    # Base Case
+    if n == 1 :
+        return 1
+
+    # maxEndingHere is the length of LIS ending with arr[n-1]
+    maxEndingHere = 1
+
+    """Recursively get all LIS ending with arr[0], arr[1]..arr[n-2]
+       IF arr[n-1] is maller than arr[n-1], and max ending with
+       arr[n-1] needs to be updated, then update it"""
+    for i in xrange(1, n):
+        res = dynamicLoop(arr , i)
+        if arr[i-1] < arr[n-1] and res+1 > maxEndingHere:
+            maxEndingHere = res +1
+
+    # Compare maxEndingHere with overall maximum. And
+    # update the overall maximum if needed
+    maximum = max(maximum , maxEndingHere)
+
+    return maxEndingHere
+
+
 def runLgis(inputFile):
     fi = open(inputFile, 'r')
     inputData, permArray = fi.readline(), fi.readline().strip("\n").split(" ")
     highestPath, lowestPath = [], []
+    global maximum
+    maximum = 1
+    dynamicLoop(permArray, len(permArray))
 
-    #to find highest path
-    for index, k1 in enumerate(permArray):
-        tempHigh = [k1]
-        print "yuhuuu" + str(tempHigh)
-        for k2 in permArray[index:]:
-            print k2 + " : " + str(int(tempHigh[-1]) + 1)
-            if k2 == str(int(tempHigh[-1]) + 1):
-                tempHigh.append(k2)
-        if len(tempHigh) > len(highestPath):
-            highestPath = tempHigh
-
-    #to find lowest path
-
-
-    return highestPath
+    return maximum
 
 
 #-------------------------------------------------------------------------------
